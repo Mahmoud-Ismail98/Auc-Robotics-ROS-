9dc1c104fac3420b90de9cb192b821d3654933cf
Mahmoud-koraim <m.koraim77@gmail.com>
2020-08-13 22:23:50 +0200

    task2,3 create my pkg auc_ws and add my name and email to package.xml

---
+++ day3/korayem_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/korayem/AucRobotics/day3/korayem_ws/src
+# Build directory: /home/korayem/AucRobotics/day3/korayem_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ day3/korayem_ws/build/gtest/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/korayem/AucRobotics/day3/korayem_ws/build/gtest/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
