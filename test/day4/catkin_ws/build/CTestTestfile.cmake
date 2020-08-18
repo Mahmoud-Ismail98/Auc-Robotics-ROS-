68b3880d7d3f9ffdce9452ec19dd8387ca17be35
Mahmoud-koraim <m.koraim77@gmail.com>
2020-08-18 15:20:13 +0200

    test node

---
+++ test/day4/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/korayem/test/lec/day4/catkin_ws/src
+# Build directory: /home/korayem/test/lec/day4/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(light_robot)
