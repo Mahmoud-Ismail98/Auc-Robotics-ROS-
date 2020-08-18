d448f832053f3935f7911c440e63b5cc0a957f82
Mahmoud-koraim <m.koraim77@gmail.com>
2020-08-18 17:29:40 +0200

    test_node_python

--- test/day4/catkin_ws/build/light_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/korayem/test/lec/day4/catkin_ws/src/light_robot
-# Build directory: /home/korayem/test/lec/day4/catkin_ws/build/light_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
